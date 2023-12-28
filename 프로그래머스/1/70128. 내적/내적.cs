2b77d5f6fa605db8335b6ea6ee2918e88e62e315
Cheon Hong <107188519+hhhhhongg@users.noreply.github.com>
2023-12-28 09:09:44 +0900

    [level 1] Title: 내적, Time: 0.16 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/70128. 내적/내적.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(int[] a, int[] b) {
+        int answer = 1234567890;
+        int sum = 0;
+        for(int i = 0; i < a.Length; i++)
+        {
+            sum += a[i] * b[i];   
+        }
+        answer = sum;
+        return answer;
+    }
+}
