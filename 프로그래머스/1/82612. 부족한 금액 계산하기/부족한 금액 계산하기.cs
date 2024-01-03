c8e3b19c3ee6e8482c33b1799a85d2244b3648e6
Cheon Hong <107188519+hhhhhongg@users.noreply.github.com>
2024-01-03 11:12:49 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.16 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Solution
+{
+    public long solution(int price, int money, int count)
+    {
+        long sum = 0;
+        for(int i = 0; i <= count; i++ )
+        {
+            sum += price * i;        
+        }
+        
+        if(sum > money)
+        {
+            return sum - money;
+        }
+        else
+        {
+            return 0;    
+        }
+    }
+    
+}
